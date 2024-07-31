e32b2b31cc544e65495ab0d66db6c48c45227250
LucasFiorentin <lucasfiorentin2008@gmail.com>
2024-07-30 21:49:16 -0300

    Criacao Component Menu

---
+++ src/Menu.tsx
@@ -0,0 +1,43 @@
+import heart from './img/heart.png'
+import notification from './img/notification.png'
+import setting from './img/setting-2.png'
+import image from './img/Image.png'
+
+import { Search, SlidersHorizontal } from 'lucide-react'
+
+function Menu() {
+  return (
+    <div className="flex place-items-center p-10  pl-12 mt-2 justify-between bg-[#F6F7F9] rounded-3xl shadow-2xl">
+      <div className="flex flex-1 gap-20">
+        <h1 className="text-[#3563E9] text-3xl font-bold ">MORENT </h1>
+        <div className="flex-1">
+          <Search className="relative top-9 left-2 -mt-6 text-[#596780]"></Search>
+          <input
+            className='"border border-gray-300 rounded-full pr-12 p-3 pl-10 w-full max-w-[500px] border-0 focus:border-0 focus:outline-transparent focus:ring bg-[#C3D4E9]"'
+            type="text"
+            name=""
+            id=""
+            placeholder="Search something here"
+          />
+          <SlidersHorizontal className="relative -top-3 left-[460px] -mt-6 text-[#596780]"></SlidersHorizontal>
+        </div>
+      </div>
+      <div className="flex place-items-center gap-8 ">
+        <div className="border border-[#596780]-300 p-2 rounded-full mb-4">
+          <img src={heart} alt="" />
+        </div>
+        <div className="border border-[#596780]-300 p-2 rounded-full mb-4">
+          <img src={notification} alt="" />
+        </div>
+        <div className="border border-[#596780]-300 p-2 rounded-full mb-4">
+          <img src={setting} alt="" />
+        </div>
+        <div className="border border-[#596780]-300  rounded-full mb-4">
+          <img src={image} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
